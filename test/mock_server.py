e5847c9887e1565278ff5f23934a0dd38e6810d5
azuki774s <azuki774s@gmail.com>
2024-08-11 23:43:01 +0900

    add GET mock

---
+++ test/mock_server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MockHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response = [
+            {
+                "id": 2,
+                "category_id": 210,
+                "category_name": "カテゴリ2",
+                "datetime": "2019-08-26T14:15:22Z",
+                "from": "ふぉーむ２",
+                "type": "タイプ２",
+                "price": 200,
+                "memo": "メモ２"
+            },
+            {
+                "id": 1,
+                "category_id": 200,
+                "category_name": "カテゴリ1",
+                "datetime": "2019-08-24T14:15:22Z",
+                "from": "ふぉーむ１",
+                "type": "タイプ１",
+                "price": 1000,
+                "memo": "メモ１"
+            },
+        ]
+        responseBody = json.dumps(response)
+
+        self.wfile.write(responseBody.encode('utf-8'))
+
+def import_args():
+    parser = argparse.ArgumentParser("mock server start")
+
+    parser.add_argument('--host', '-H', required=False, default='0.0.0.0')
+    parser.add_argument('--port', '-P', required=False, type=int, default=8080)
+
+    args = parser.parse_args()
+
+    return args.host, args.port
+
+def run(server_class=HTTPServer, handler_class=MockHandler, server_name='localhost', port=20010):
+
+    server = server_class((server_name, port), handler_class)
+    server.serve_forever()
+
+def main():
+    host, port = import_args()
+    run(server_name=host, port=port)
+
+if __name__ == '__main__':
+    main()
