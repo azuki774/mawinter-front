787294feb2ce8b53630f525a33b3cd1b91fb5ef6
azuki774s <azuki774s@gmail.com>
2024-08-12 13:16:30 +0900

    fix table columns

--- components/History.vue
+++ components/History.vue
@@ -13,8 +13,13 @@ const asyncData = await useAsyncData(
 );
 
 const data = asyncData.data.value as Record[];
+
+// fetchデータを整形
+for (let d of data) {
+  d.datetime = d.datetime.slice(0, 19); // 2023-09-23T00:00:00+09:00 -> 2023-09-23T00:00:00
+}
+
 recordList.value = data
-console.log(data)
 
 </script>
 
@@ -24,6 +29,7 @@ console.log(data)
       <thead>
         <tr>
           <th>ID</th>
+          <th>カテゴリ名</th>
           <th>金額</th>
           <th>日付</th>
           <th>メモ</th>
@@ -32,8 +38,9 @@ console.log(data)
       <tbody>
         <tr v-for="record in recordList">
           <td>{{ record.id }}</td>
+          <td>{{ record.category_name }}</td>
           <td>{{ record.price }}</td>
-          <td>{{ record.date }}</td>
+          <td>{{ record.datetime }}</td>
           <td>{{ record.memo }}</td>
         </tr>
       </tbody>
