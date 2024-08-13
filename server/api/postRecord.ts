7002004367e9ffa5f2fedbdf11600f241dded305
azuki774s <azuki774s@gmail.com>
2024-08-13 23:06:06 +0900

    create reqbody manually

--- server/api/postRecord.ts
+++ server/api/postRecord.ts
@@ -2,11 +2,15 @@ export default defineEventHandler ( async (event) => {
   const config = useRuntimeConfig()
   const query = getQuery(event)
   // create body
-  const reqbody = {
-    'category_id': query.category_id,
-    'price': query.price,
-    'from': "mawinter-fe"
+  const reqbody = `
+  {
+  "category_id":${query.category_id},
+  "price":${query.price},
+  "from":"mawinter-fe",
+  "memo":"手動"
   }
+  `
+  console.log(reqbody)
   const url = config.public.mawinterApi + "/v2/record"
   const result = await $fetch(url,
     {
