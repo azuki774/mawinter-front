90c18f8ca6bd0dcacc9abb7b1414b3cdacf8ed8d
azuki774s <azuki774s@gmail.com>
2024-08-13 21:48:05 +0900

    wip: add server proxy

--- components/PostRecord.vue
+++ components/PostRecord.vue
@@ -9,19 +9,43 @@ const asyncData = await useAsyncData(
     const response = $fetch(url);
     return response;
   }
-  );
+);
 
 const data = asyncData.data.value as Category[];
 categoryList.value = data
+
+// pricebox
+const priceBox = ref<number>()
+
+const postButton = async (): Promise<void> => {
+  console.log('start')
+  const asyncData = await useAsyncData(
+    `record`,
+    (): Promise<any> => {
+      const url = "/api/postRecord"
+      const response = $fetch(url);
+      console.log('response')
+      return response;
+    }
+  );
+};
 
 </script>
 
 <template>
   <section>
+
     <select name="categorySelector">
       <option v-for="category in categoryList" :key="category.category_id">
         {{ category.category_name }}
       </option>
     </select>
+
+    <input v-model="priceBox" type="number" />
+
+    <div class="col">
+      <button @click="postButton" name="postButton" class="sendbutton" type="submit">Post</button>
+    </div>
+
   </section>
 </template>
