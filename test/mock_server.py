222558b81852bcbea528a4a543d1c04bd57d2415
azuki774s <azuki774s@gmail.com>
2024-08-13 22:21:30 +0900

    fix endpoint name

--- test/mock_server.py
+++ test/mock_server.py
@@ -6,7 +6,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class MockHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/v2/records":
+        if self.path == "/v2/record":
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
@@ -60,7 +60,7 @@ class MockHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         self.send_response(201)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
 
 def import_args():
