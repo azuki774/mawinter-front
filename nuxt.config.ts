cd253f919e96992fc6ecf4919e046976deb44f24
azuki774s <azuki774s@gmail.com>
2024-08-16 20:57:25 +0900

    :Revert "install bootstrap"
    This reverts commit 1f7f901ebcccb6e1d11783b4c6a6508befdc898c.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-
   routeRules: {
     "/": { ssr: false },
   },
-
   runtimeConfig: {
     public: { // 外部から取得するにはpublic が必要
       mawinterApi: "http://mawinter-api", // .env の NUXT_PUBLIC_API_BASE_ENDPOINT から取得
     }
-  },
-
-  modules: ["usebootstrap"]
+  }
 })
