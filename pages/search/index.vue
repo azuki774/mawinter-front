a6a84a461bdfe8497e88f87b340bbf32df7ce8fe
azuki774s <azuki774s@gmail.com>
2025-02-11 11:52:50 +0900

    add routing for searching history

---
+++ pages/search/index.vue
@@ -0,0 +1,20 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+  <SearchHistory />
+</template>
+
+<style lang='css'>
+h2 {
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.summary_link {
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
