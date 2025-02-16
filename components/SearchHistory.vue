f93562b65019e7f6edac8debfb7572fa820f2aaa
azuki774s <azuki774s@gmail.com>
2025-02-16 22:36:48 +0900

    TODO: getHistories

--- components/SearchHistory.vue
+++ components/SearchHistory.vue
@@ -1,34 +1,40 @@
 <script setup lang='ts'>
 import { ref } from 'vue'
+import type { Record } from '@/interfaces'
 import type { Header, Item } from 'vue3-easy-data-table'
+
+const asyncData = await useFetch(
+  '/api/getHistories?yyyymm=202502',
+  {
+    key: `/api/getHistories?yyyymm=202502`,
+  },
+)
+
+const data = asyncData.data.value as Record[]
+
+// fetchデータを整形
+if (data != undefined) { // 取得済の場合のみ
+  for (const d of data) {
+    d.datetime = d.datetime.slice(0, 19) // 2023-09-23T00:00:00+09:00 -> 2023-09-23T00:00:00
+  }
+}
 
 const headers = ref<Header[]>([
   { text: 'ID', value: 'id' },
   { text: 'カテゴリ名', value: 'category_name', sortable: true },
-  { text: '利用日', value: 'datetime' },
+  { text: '利用日', value: 'datetime', sortable: true },
   { text: '登録元', value: 'from' },
   { text: '金額', value: 'price', sortable: true },
   { text: 'メモ', value: 'memo' },
 ])
 
-const items = ref<Item[]>([
-  { id: '1', category_name: 'カテゴリ名', datetime: '2025-01-01', from: 'テスト', price: 1234, memo: 'メモ欄' },
-])
-
-const addItem = () => {
-  items.value = [
-    { id: '2', category_name: '新しいカテゴリ', datetime: '2025-02-01', from: 'テスト2', price: 5678, memo: '新しいメモ' },
-    { id: '3', category_name: '別のカテゴリ', datetime: '2025-03-01', from: 'テスト3', price: 91011, memo: '別のメモ' },
-  ]
-}
+const items = ref<Item[]>(data)
 
 </script>
 
 <template>
-    <div class='container text-center'>
       <div class='row justify-content-center'>
-        <EasyDataTable :headers="headers" :items="items" />
-        <button @click="addItem">アイテムを追加</button>
-      </div>
+      <h2>レコード</h2>
+      <EasyDataTable :headers="headers" :items="items" />
     </div>
 </template>
