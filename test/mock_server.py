ede0f000fd8c1e30cec2d3ef515c982e2452c7c5
azuki774s <azuki774s@gmail.com>
2025-02-16 23:57:04 +0900

    todo: double query params

--- test/mock_server.py
+++ test/mock_server.py
@@ -6,7 +6,7 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class MockHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/v2/record":
+        if self.path.startswith("/v2/record"):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
