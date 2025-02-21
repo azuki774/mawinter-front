fe6aa0065ff9cf534acfa3861a2ad56e86e13670
azuki774s <azuki774s@gmail.com>
2025-02-21 17:48:12 +0900

    add interface api

--- server/api/getHistories.ts
+++ server/api/getHistories.ts
@@ -1,13 +1,14 @@
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   const query = getQuery(event)
-  const params: {
-    yyyymm: string
-    category_id: string
-  } = {
-    yyyymm: String(query.yyyymm),
-    category_id: String(query.category_id),
+  const params: { [key: string]: string } = {}
+  if (query.yyyymm != undefined) {
+    params.yyyymm = String(query.yyyymm)
   }
+  if (query.category_id != undefined) {
+    params.category_id = String(query.category_id)
+  }
+
   const queryParams = new URLSearchParams(params)
   const url = config.public.mawinterApi + '/v2/record?' + queryParams
   const result = await $fetch(url,
