b73494ff9395b3b313f9f5080e0a9ad2cfadca4e
azuki774s <azuki774s@gmail.com>
2025-02-21 15:51:43 +0900

    add selected_yyyymm_Value dropdown menu

--- components/SearchHistory.vue
+++ components/SearchHistory.vue
@@ -30,11 +30,40 @@ const headers = ref<Header[]>([
 
 const items = ref<Item[]>(data)
 
+const options_yyyymm = ref([
+  { value: '202502', label: '202502' },
+  { value: '202501', label: '202501' },
+  { value: '202412', label: '202412' },
+])
+
+const selected_yyyymm_Value = ref('')
+
+// 選択変更時に実行する処理
+watch(selected_yyyymm_Value, (newValue) => {
+  const fetchData = useFetch(
+    '/api/getHistories?yyyymm=' + newValue + '&category_id=200', // TODO
+    {
+      key: `/api/getHistories`,
+    },
+  )
+  items.value = fetchData.data.value as Record[]
+})
+
 </script>
 
 <template>
       <div class='row justify-content-center'>
       <h2>レコード</h2>
+      <div>
+        <label for="dropdown">取得月:</label>
+        <select id="dropdown" v-model="selected_yyyymm_Value">
+          <option value="" disabled>選択してください</option>
+          <option v-for="option in options_yyyymm" :key="option.value" :value="option.value">
+            {{ option.label }}
+          </option>
+        </select>
+        <p>選択された値: {{ selected_yyyymm_Value }}</p>
+      </div>
       <EasyDataTable :headers="headers" :items="items" />
     </div>
 </template>
